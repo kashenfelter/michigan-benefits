11460c24b15aaa9b9cda67873ba5cb8974802cf8
Christa Hartsock <christa@codeforamerica.org>
2018-04-26 13:39:52 -0700

    Convert job_count into an employments relation
    Previously, we asked users for how many jobs they had. We now need to collect additional information about those jobs, so this commit creates an Employment relation on HouseholdMember and retrofits the relevant combined applicaition questions to use them.
    
    * Creates migration to add polymorphic `application_member` to `employments`, so that employments can be association with both Member (old flows) and HouseholdMember (new flow)
    * Creates rake task to convert old Employment into new polymorphic Employment, and to create new employments for each job in job_count
    * Converts `job_count` from integer to instead creating that number of empty employments in flow
    * Adds support for array field in mb_incrementer (eg. `{ job_counts: '1', '2' }`)
    
    [#156390039]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425213023) do
+ActiveRecord::Schema.define(version: 20180425222025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,13 +149,16 @@ ActiveRecord::Schema.define(version: 20180425213023) do
   end
 
   create_table "employments", force: :cascade do |t|
+    t.bigint "application_member_id"
+    t.string "application_member_type"
     t.datetime "created_at", null: false
     t.string "employer_name"
     t.integer "hours_per_week"
-    t.integer "member_id", null: false
+    t.integer "member_id"
     t.string "pay_quantity"
     t.string "payment_frequency"
     t.datetime "updated_at", null: false
+    t.index ["application_member_id"], name: "index_employments_on_application_member_id"
   end
 
   create_table "expenses", force: :cascade do |t|
